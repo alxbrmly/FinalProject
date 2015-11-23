b2c96b520039faecf259da9cc3c2b6c11c442243
Alex Bromley <alexandrabromley@gmail.com>
2015-11-22 23:57:44 -0500

    changed photo on shop page, need to remove links div

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 // $('h5').fadeOut(10000);
 
 
-$('h4').animate( { 
-  opacity: 0.25,   
-  width: "70%" } , 2000 );
+$('h4').animate({
+    opacity: 0.25,
+    width: "70%"
+}, 2000);
 
-$('h5').animate( { 
-  opacity: 0.25,   
-  width: "70%" } , 5000 );
+$('h5').animate({
+    opacity: 0.25,
+    width: "70%"
+}, 5000);
 
-$('h6').animate( { 
-  opacity: 0.40,   
-  width: "70%" } , 5000 );
-
+$('h6').animate({
+    opacity: 0.40,
+    width: "70%"
+}, 5000);
